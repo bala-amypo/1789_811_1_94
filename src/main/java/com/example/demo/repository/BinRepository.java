bf98e98ac2079f9e401fb094459c6157e09e32f6
bala-amypo <bala@amypo.in>
2025-12-20 10:19:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/BinRepository.java
+++ src/main/java/com/example/demo/repository/BinRepository.java
@@ -5,9 +5,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.example.demo.model.Bin;
 import com.example.demo.model.Zone;
-
 @Repository
 public interface BinRepository extends JpaRepository<Bin, Long> {
 
--- src/main/java/com/example/demo/repository/ZoneRepository.java
+++ src/main/java/com/example/demo/repository/ZoneRepository.java
@@ -3,9 +3,9 @@ package com.example.demo.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Zone;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
